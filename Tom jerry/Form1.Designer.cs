0e428d0674faa2bcb8854cbd11a7928bd71886c4
Baxsh_cw07@itstep.edu.az <Baxsh_cw07@itstep.edu.az>
2023-02-07 10:54:38 +0400

    Add project files.

---
+++ Tom jerry/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Tom_jerry
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.jerrybtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // jerrybtn
+            // 
+            this.jerrybtn.BackColor = System.Drawing.Color.Red;
+            this.jerrybtn.Location = new System.Drawing.Point(111, 68);
+            this.jerrybtn.Name = "jerrybtn";
+            this.jerrybtn.Size = new System.Drawing.Size(57, 56);
+            this.jerrybtn.TabIndex = 0;
+            this.jerrybtn.UseVisualStyleBackColor = false;
+            this.jerrybtn.MouseMove += new System.Windows.Forms.MouseEventHandler(this.jerrybtn_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(958, 501);
+            this.Controls.Add(this.jerrybtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button jerrybtn;
+    }
+}
+
---
+++ Tom jerry/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tom_jerry.Properties;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Header;
+
+namespace Tom_jerry
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void jerrybtn_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(sender is Button b)
+            {
+                b.Dispose();
+            }
+            Button newjerrybtn = new Button();
+            newjerrybtn.BackColor = Color.Red;
+            newjerrybtn.FlatStyle = FlatStyle.Flat;
+            newjerrybtn.Size = jerrybtn.Size;
+            newjerrybtn.FlatAppearance.BorderSize = newjerrybtn.FlatAppearance.BorderSize;
+            newjerrybtn.FlatAppearance.BorderColor = newjerrybtn.FlatAppearance.BorderColor;
+            newjerrybtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            newjerrybtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            Random random = new Random();
+            int x = random.Next(10, 800);
+            int y = random.Next(10, 500);
+            newjerrybtn.Location = new Point(x, y);
+            newjerrybtn.AutoSize = true; ;
+            newjerrybtn.MouseMove += jerrybtn_MouseMove;
+            this.Controls.Add(newjerrybtn);
+        }
+    }
+}
